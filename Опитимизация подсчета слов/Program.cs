4ef1319daeb5bdbc4a196ffed075426c267e89f4
79512694486 <79512694486@DESKTOP-HJOBT07>
2024-12-13 18:40:00 +0500

    Добавьте файлы проекта.

---
+++ Опитимизация подсчета слов/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = "Это пример текста. Текст должен быть проанализирован. Это важно.";
+        var wordCounts = WordFrequency(text);
+
+        foreach (var item in wordCounts)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+    }
+
+    static Dictionary<string, int> WordFrequency(string text)
+    {
+        var words = Regex.Matches(text, @"\b\w+\b");
+        var result = new Dictionary<string, int>();
+
+        foreach (Match match in words)
+        {
+            string word = match.Value;
+
+            if (result.TryGetValue(word, out int count))
+            {
+                result[word] = count + 1;
+            }
+            else
+            {
+                result[word] = 1;
+            }
+        }
+
+        return result;
+    }
+}
+
+
+
+
+
